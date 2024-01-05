e8bf4f5814768fc0a31f07b751238208d91ab1a2
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-01-04 21:44:35 -0500

    Improve JokeQuestion component by returning cleanup function only if message is still animating out

--- src/joke-overlay/JokeQuestion.tsx
+++ src/joke-overlay/JokeQuestion.tsx
@@ -4,11 +4,13 @@ function JokeQuestion({ question }: { question: string; key: string }) {
   const [index, setIndex] = useState(0);
   const displayedQuestion = question.slice(0, index);
   useEffect(() => {
-    const timeout = setTimeout(() => {
-      if (index < question.length) setIndex((oldIndex) => oldIndex + 1);
-    }, 50);
-    return () => clearTimeout(timeout);
-  }, [question, index]);
+    if (index < question.length) {
+      const timeout = setTimeout(() => {
+        setIndex((oldIndex) => oldIndex + 1);
+      }, 50);
+      return () => clearTimeout(timeout);
+    }
+  }, [index]);
   return (
     <h1 className="w-3/4 text-center text-2xl md:text-3xl lg:text-7xl lg:leading-snug">
       <span>{displayedQuestion}</span>
