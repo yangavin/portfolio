c86a2e0e0342681c67d75ac3e3783b187c732bfd
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-01-14 21:59:50 -0500

    Refractor getProjectsCard to api

--- src/api/getProjectCards.ts
+++ src/api/getProjectCards.ts
@@ -1,10 +1,24 @@
 import { contentfulClient } from "../lib/contentful";
 import type { ProjectSkeleton } from "./models";
+import type { Logo } from "./models";
 
 async function getProjectCards() {
-  return await contentfulClient.getEntries<ProjectSkeleton>({
+  const rawProjects = await contentfulClient.getEntries<ProjectSkeleton>({
     content_type: "projectCard",
     order: ["sys.createdAt"],
+  });
+
+  return rawProjects.items.map((projectCard) => {
+    return {
+      ...projectCard.fields,
+      logo: `https:${(projectCard.fields.logo as Logo).fields.file.url}`,
+      techUsed: projectCard.fields.techUsed.map((techCard) => {
+        return {
+          ...techCard.fields,
+          logo: `https:${techCard.fields.logo.fields.file.url}`,
+        };
+      }),
+    };
   });
 }
 
