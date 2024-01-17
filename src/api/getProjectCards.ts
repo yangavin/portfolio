31e6728873b554b4f3a64fdcaa6142cbc29a950a
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-01-16 19:53:35 -0500

    Improve description styling for project cards

--- src/api/getProjectCards.ts
+++ src/api/getProjectCards.ts
@@ -1,6 +1,18 @@
 import { contentfulClient } from "../lib/contentful";
 import type { ProjectSkeleton, Asset, TechCard } from "./models";
 import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
+import type { Options } from "@contentful/rich-text-html-renderer";
+import { INLINES } from "@contentful/rich-text-types";
+
+// Render links with styles and target blank attribute
+const aTagRenderingOption: Options = {
+  renderNode: {
+    [INLINES.HYPERLINK]: (node, next) =>
+      `<a class="text-blue-500 underline" target="_blank" href=${
+        node.data.uri
+      }>${next(node.content)}</a>`,
+  },
+};
 
 async function getProjectCards() {
   const rawProjects = await contentfulClient.getEntries<ProjectSkeleton>({
@@ -18,7 +30,10 @@ async function getProjectCards() {
           logo: `https:${techCard.fields.logo.fields.file.url}`,
         };
       }),
-      description: documentToHtmlString(projectCard.fields.description),
+      description: documentToHtmlString(
+        projectCard.fields.description,
+        aTagRenderingOption,
+      ),
     };
   });
 }
