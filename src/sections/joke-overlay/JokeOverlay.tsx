4ff75f84f1dd189a4aa258d14a65f2474ab35f36
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-01-21 22:38:18 -0500

    Remove default sample joke and only render joke when data is available

--- src/sections/joke-overlay/JokeOverlay.tsx
+++ src/sections/joke-overlay/JokeOverlay.tsx
@@ -5,12 +5,8 @@ import JokePunchline from "./JokePunchline";
 type Joke = [{ question: string; punchline: string }];
 
 function JokeOverlay() {
-  const [question, setQuestion] = useState(
-    "Why couldn't web developers find their room in a hotel?",
-  );
-  const [punchline, setPunchline] = useState(
-    "Because their room number is 404",
-  );
+  const [question, setQuestion] = useState("");
+  const [punchline, setPunchline] = useState("");
   const [questionDone, setQuestionDone] = useState(false);
   const [jokeDone, setJokeDone] = useState(false);
 
@@ -35,23 +31,29 @@ function JokeOverlay() {
       return () => clearTimeout(fadeoutTimer);
     }
   }, [jokeDone]);
-
-  return (
-    <div
-      ref={overlay}
-      className="fixed z-10 flex h-full w-full flex-col items-center justify-center gap-10 bg-orange-100 lg:gap-20"
-    >
-      <JokeQuestion setDone={() => setQuestionDone(true)} key={question}>
-        {question}
-      </JokeQuestion>
-      <hr className="w-9/12 border-black" />
-      <JokePunchline
-        showCursor={questionDone}
-        setDone={() => setJokeDone(true)}
-        key={punchline}
+  if (question && punchline) {
+    return (
+      <div
+        ref={overlay}
+        className="fixed z-10 flex h-full w-full flex-col items-center justify-center gap-10 bg-orange-100 lg:gap-20"
       >
-        {punchline}
-      </JokePunchline>
+        <JokeQuestion setDone={() => setQuestionDone(true)} key={question}>
+          {question}
+        </JokeQuestion>
+        <hr className="w-9/12 border-black" />
+        <JokePunchline
+          showCursor={questionDone}
+          setDone={() => setJokeDone(true)}
+          key={punchline}
+        >
+          {punchline}
+        </JokePunchline>
+      </div>
+    );
+  }
+  return (
+    <div className="fixed z-10 flex h-full w-full flex-col items-center justify-center gap-10 bg-orange-100 lg:gap-20">
+      <hr className="w-9/12 border-black" />
     </div>
   );
 }
