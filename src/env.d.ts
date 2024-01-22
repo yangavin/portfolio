f45deb9b09c8a0e83762581ead2f4ec1d9147924
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-01-21 22:55:06 -0500

    Remove unnecessary github token env variable type

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,5 +4,4 @@ interface ImportMetaEnv {
   readonly CONTENTFUL_SPACE_ID: string;
   readonly CONTENTFUL_DELIVERY_TOKEN: string;
   readonly CONTENTFUL_PREVIEW_TOKEN: string;
-  readonly GITHUB_ACCESS_TOKEN: string;
 }
