344d981666f59563df86a453f99a69e1a45d6f44
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-01-24 23:40:17 -0500

    Install vercel ssr integration for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  output: "server",
   output: "server",
   adapter: vercel({
     webAnalytics: {
